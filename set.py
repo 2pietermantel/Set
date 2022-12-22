359f96c7dbee4dee66309c34a239f32bb58b0258
2pietermantel <2pietermantel@gmail.com>
2022-12-22 14:29:33 +0100

    Added pygame

--- set.py
+++ set.py
@@ -1,5 +1,15 @@
+from dataclasses import dataclass
+import pygame
+
+@dataclass
+class Kaart:
+    kleur : int
+    vorm : int
+    vulling : int
+    aantal : int
+
 def main():
-    
+    pygame.init()
 
 if __name__ == "__main__":
     main()
