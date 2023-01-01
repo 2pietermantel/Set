ac2ce19f3206f71bf776dbb3f47041f175a63ae3
pietermantel <2pietermantel@gmail.com>
2023-01-01 21:07:21 +0100

    Alles in één bestand gestopt
    Daarnaast ook wat abstractie rondom de visuele kaarten toegevoegd

--- set.py
+++ set.py
@@ -1,6 +1,20 @@
+import pygame
 from dataclasses import dataclass
-import game
 
+# Some variables
+WIDTH = 800 # pixels
+HEIGHT = 600 # pixels
+
+GLIDE_DURATION = 0.5 # seconds
+FPS = 60
+
+# Colors (R, G, B)
+COLOR_BACKGROUND = (221, 221, 221)
+
+game_objects = []
+total_glide_ticks = GLIDE_DURATION * FPS
+
+# === ALLES RONDOM DE LOGICA ACHTER SET ===
 @dataclass
 class Kaart:
     kleur : int
@@ -11,8 +25,110 @@ class Kaart:
     def getValues(self):
         return [self.kleur, self.vorm, self.vulling, self.aantal]
 
-def main():
-    game.initialize()
+# === ALLES RONDOM HET GUI ===
+def initialize():
+    pygame.init()
+    
+    vc = SetCard(position = (300, 100), card = Kaart(2,3,3,1))
+    
+    vc.glide((200, 200))
+    game_objects.append(vc)
+    
+    loop()
+    
+def loop():
+    # Initialize screen
+    clock = pygame.time.Clock()
+    canvas = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Set!")
+    
+    running = True
+    while running:
+        # Check if the game should quit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+        # ticking
+        for game_object in game_objects:
+            game_object.tick()
+        
+        # Rendering
+        canvas.fill(COLOR_BACKGROUND)
+        
+        for game_object in game_objects:
+            game_object.render(canvas)
+        
+        pygame.display.update()
+        
+        clock.tick(FPS)
+                
+    pygame.quit()
 
+# === GAME OBJECTS ===
+class VisualCard:
+    def __init__(self, position = (0, 0), filename = "blank"):
+        self.position = position
+        self.gliding = False
+            
+        self.texture = pygame.image.load(f"kaarten\\{filename}.gif")
+        
+    def tick(self):
+        # Handle Glide Animation
+        if self.gliding:
+            self.glide_animation.tick()
+            self.position = self.glide_animation.getCurrentPosition()
+            if self.glide_animation.isFinished():
+                self.gliding = False
+        
+    def render(self, canvas):
+        canvas.blit(self.texture, self.position)
+        
+    def glide(self, new_position : tuple):
+        self.gliding = True
+        self.glide_animation = GlideAnimation(self.position, new_position)
+        
+class SetCard(VisualCard):
+    def __init__(self, position = (0, 0), card = Kaart(1, 1, 1, 1)):
+        # Get the filename by getting the values as a list, converting those values to strings and joining it together
+        filename = "".join([str(x) for x in card.getValues()])
+        
+        super().__init__(position, filename)
+
+# === OTHER OBJECTS
+@dataclass
+class GlideAnimation:
+    begin : tuple
+    end : tuple
+    current_tick : int
+    
+    def __init__(self, begin, end, current_tick = 0):
+        self.begin = begin
+        self.end = end
+        self.current_tick = current_tick
+        
+    def tick(self):
+        self.current_tick += 1
+        
+    def getCurrentPosition(self):
+        # TODO: Betere animatie
+        # self.begin en self.end zijn de begin- en eindcoördinatien als tuple
+        # self.current_tick is op welke frame de animatie is
+        # total_glide_time is de totale tijd die de animatie moet duren
+        # return een tuple met de coördinaten op het huidige moment
+        
+        # voor nu is het een beweging met constante snelheid, maar het moet iets vloeiender worden
+        
+        dx = self.end[0] - self.begin[0]
+        dy = self.end[1] - self.begin[1]
+        dt = self.current_tick / total_glide_ticks
+        x = self.begin[0] + dx * dt
+        y = self.begin[1] + dy * dt
+        return (x, y)
+    
+    def isFinished(self):
+        return self.current_tick >= total_glide_ticks
+    
+# Start het spel
 if __name__ == "__main__":
-    main()
+    initialize()
